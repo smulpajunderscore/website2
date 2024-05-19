fb1132682b7d3628c615b7e0130c5bb28d7d7ddc
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-05-19 01:07:38 +0000

    Sat, May 18, 2024, 6:07 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -5,7 +5,7 @@ const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
-const word =['application', 'programming', 'interface', 'wizard']
+const word = ['application', 'programming', 'interface', 'wizard']
 
 let selectedIndex = Math.floor (word.length * Math.random())
 let selectedWord = word[selectedIndex]
