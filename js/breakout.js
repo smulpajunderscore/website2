2d2fd1dd6c6d4133840cdad24962aee5199a32c4
Jeremiah Luna <142760027+smulpajunderscore@users.noreply.github.com>
2024-05-22 04:31:16 +0000

    Tue, May 21, 2024, 9:31 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -2,6 +2,7 @@ rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
 startBtn = document.getElementById('start-btn')
+gameRunning = true
 
 // game interaction
 canvas = document.getElementById('canvas')
@@ -133,7 +134,7 @@ function moveBall() {
     }
     if (ball.y + ball.size > canvas.height) {
         ball.dy = -1 * ball.dy
-        showAllBricks()
+        gameRunning = false
     }
 
     if (ball.x + ball.size > canvas.width) {
@@ -184,6 +185,9 @@ function showAllBricks() {
 // draw new frame
 
 function update() {
+    if gameRunning = false {
+        return 0;
+    }
     moveBall()
     movePaddle()
     draw()
@@ -201,6 +205,7 @@ closeBtn.addEventListener('click', () => {
 
 startBtn.addEventListener('click', () => {
     update()
+
     element = document.getElementById("start-btn")
     ball.x = 4
     ball.y = -4
